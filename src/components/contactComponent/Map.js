c393de01ff3d07126b648949227d4f70bcee0634
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-02-16 06:23:31 -0800

    this is location component

---
+++ src/components/contactComponent/Map.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '@mui/material/Container'
+
+const Map = () => {
+  return (
+    <Container maxWidth="xl">
+      
+    
+      
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.3378793195666!2d38.66375611432647!3d9.03291049141593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b8765802bd65f%3A0xe0dcc551554e8f40!2zU2hha2EgTWFsbCB8IEFzaGV3YSBNZWRhIHwg4Yi74YmDIOGInuGIjSB8IOGKoOGIuOGLiyDhiJzhi7M!5e0!3m2!1sam!2set!4v1675943325096!5m2!1sam!2set" width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </Container>
+  )
+}
+
+export default Map
