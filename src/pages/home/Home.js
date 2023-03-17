a20f633acfd1fef9c82b95394ed6a0a50c07710e
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-03-16 21:30:10 -0700

    fix swiper js error

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import Aboutus from "../../components/aboutus/Aboutus"
 import Contact from "../../components/contact/Contact"
 import Hero from "../../components/hero/Hero"
-import Swiper from "../../components/hero/swiper"
+// import Swiper from "../../components/hero/swiper"
 import Mediaindex from "../../components/media/mediaIndex"
 import Products from "../../components/products.js/Products"
 import ServHead from "../../components/servicesHead/ServHead"
