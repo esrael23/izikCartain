a606b321d0c1416eb7db3fc8df93d573454a3fcc
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-03-17 00:51:38 -0700

    works title change

--- src/components/ourWorkComponent/ourWorkheader.js
+++ src/components/ourWorkComponent/ourWorkheader.js
@@ -8,7 +8,7 @@ const OurWorkheader = () => {
     <Box height={"50vh"} width="100%" sx={{backgroundImage: `url(${Bgimage})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment:'fixed'}}>
         <Box  height={"50vh"} width="100%" sx={{background:'black', opacity:'0.3', display:'flex',
         justifyContent:'center', alignItems: 'center'}} >
-          <Typography variant="h2" color={'white'} sx={{opacity:'1'}}>About us</Typography>
+          <Typography variant="h2" color={'white'} sx={{opacity:'1'}}>Our Works</Typography>
         </Box>
       </Box>
   )
