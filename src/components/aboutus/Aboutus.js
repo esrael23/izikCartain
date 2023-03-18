575217baa45d16763e64149769e7fa8ebc8c08b0
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-03-18 02:38:59 -0700

    aboutUs js change fontSize

--- src/components/aboutus/Aboutus.js
+++ src/components/aboutus/Aboutus.js
@@ -9,12 +9,12 @@ const Aboutus = () => {
     <>
     <ThemeProvider theme={themes}>
 
-    <Container maxWidth="md">
+    <Container maxWidth="lg">
       <Box height={"50vh"} width={'100%'} sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
         <Box textAlign={'center'}>
-          <Typography variant="h5" align='center' pb={4} fontWeight="300" sx={{ display: {xs:{fontSize: '50px'}} }} >Izik Curtain is a well established production workshop which has been operating in Ethiopia since 1998 EC. The company is engaged in the production of in house ,office and hotel curtains in accordance with the concept of interior design.</Typography>
-          <Button variant="outlined" size='large' color='primary' >Read more about us
-            
+          <Typography variant="h6" align='center' p={2} fontWeight="300" fontFamily={'poppins[300]'}  sx={{ fontSize: {xs: '15px', md: '20px', xl: '20px'} }} >Izik Curtain is a well established production workshop which has been operating in Ethiopia since 1998 EC. The company is engaged in the production of in house office and hotel curtains in accordance with the concept of interior design.</Typography>
+          <Button variant="outlined" size='small'>
+            Read more about us
           </Button>
           
         </Box>
