f6be58eb9b6c3f46183a329605c5d7d627313177
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-03-24 02:53:29 -0700

    i gave padding

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -8,10 +8,10 @@ import Bgimage from '../../image/hero.jpg'
 const ServHead = () => {
   return (
     <div>
-      <Box height={"50vh"} width="100%" sx={{backgroundImage: `url(${Bgimage})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment:'fixed'}}>
+      <Box height={"50vh"} width="100%" sx={{backgroundImage: `url(${Bgimage})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundAttachment:'fixed', marginBottom:{xs:'60px',sm:'50px', md: '5px'}}}>
         <Box  height={"50vh"} width="100%" sx={{background:'black', opacity:'0.3', display:'flex',
-        justifyContent:'center', alignItems: 'center'}} >
-          <Typography variant="h2" color={'white'} sx={{opacity:'1'}}>About us</Typography>
+        justifyContent:'center', alignItems: 'center', marginBottom:'50px'}} >
+          <Typography variant="h2" color={'white'} sx={{opacity:'1'}} >About us</Typography>
         </Box>
       </Box>
       <Aboutdetail/>
