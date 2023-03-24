b6d4c3dfa1150065d1f714d4aaa8e99f99cb5ef8
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-03-24 02:18:48 -0700

    edit media component 1

--- src/components/media/MediaMobile.js
+++ src/components/media/MediaMobile.js
@@ -1,15 +1,40 @@
 
 
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, IconButton, Stack, Typography } from '@mui/material'
 import Mediaimg from '../../image/Untitled-26.jpg'
 import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TelegramIcon from '@mui/icons-material/Telegram';
 
 const MediaMobile = () => {
   return (
     <div>
-      <Box width={'100%'} height={"80vh"} sx={{backgroundImage: `url(${Mediaimg})`, backgroundPosition:'center', backgroundSize: 'cover',backgroundRepeat:'no-repeat'}}>
-        <Box sx={{display: 'flex', justifyContent: 'center'}}>
-          <Typography variant="h4" color="primary">FOLLOW US </Typography>
+      <Box width={'100%'} height={"80vh"} sx={{backgroundImage: `url(${Mediaimg})`, backgroundPosition:'center', backgroundSize: 'cover',backgroundRepeat:'no-repeat', backgroundAttachment:'fixed', display: 'flex'}}>
+        <Box  width={'100%'} height={"40vh"}>
+        <Box  sx={{opacity:"0.5",
+      background:'white' , paddingLeft: '40px', textAlign: 'center'  }} >
+
+          <Typography variant="h4" color="black" sx={{marginTop: '230px', paddingLeft:'10px'}}>FOLLOW US </Typography>
+          <Box sx={{display: 'flex', alignItems: 'center',}}>
+            <Stack direction={'row'} gap={2} display="flex" justifyContent={"space-around"} >
+              <IconButton aria-label="" color='black'sx={{bgcolor: 'black'}}  size="100px">
+                
+              <FacebookIcon sx={{fontSize:'50px'}}/>
+              </IconButton>
+              <IconButton aria-label="" color='black' size="100px">
+              <InstagramIcon sx={{fontSize:'50px'}}/>
+              </IconButton>
+              <IconButton aria-label="" color='black' size="100px">
+              <TelegramIcon sx={{fontSize:'50px'}}/>
+              </IconButton>
+              <IconButton href='https://www.tiktok.com/@ikiz_curtain?_t=8auGvVIqJaX&_r=1'>
+                   <img style={{color: 'white', fontSize:'40px'}} src="https://img.icons8.com/ios/50/000000/tiktok--v1.png"/>
+                   
+                   </IconButton>
+            </Stack>
+          </Box>
+        </Box>
         </Box>
 
       </Box>
