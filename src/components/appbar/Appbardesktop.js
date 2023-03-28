04605bf62f485de43264f72906acfab2783122db
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-03-28 00:14:23 -0700

    add logo for desktop

--- src/components/appbar/Appbardesktop.js
+++ src/components/appbar/Appbardesktop.js
@@ -1,51 +1,27 @@
-
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Logo from '../../image/izikpng.png'
-import { Box, Button, Stack, Container, Avatar } from '@mui/material'
-
+import Logo from '../../image/ikizLogo1.png'
+import { Button, Stack, Container } from '@mui/material'
 const Appbardesktop = () => {
   return (
     <>
-    <AppBar position="fixed" color="grey">
-      <Container maxWidth="xl">
-        <Toolbar sx={{justifyContent: 'space-between'}}>
-        {/* <Box sx={{
-          backgroundImage: `url(${Logo})`,
-          backgroundSize:'cover',
-          backgroundPosition: 'center',
-          height: 100
-      }}>
-          
-        </Box> */}
-        {/* <Avatar  sx={{backgroundImage: `url(${Logo})`,
-          backgroundSize:'cover',
-           backgroundPosition: 'center',
-           backgroundRepeat: 'no-repeat',
-            width: '350px',
-             height: '200px'}}/> */}
-             <Typography variant="h4" color="initial">izik</Typography>
-        
-        
-        
-        <Stack direction={'row'} spacing={2} >
-          <Button
-           href='/'
-            sx={{fontSize:'15px', fontWeight: 'none'}}
-             >home</Button>
-          <Button href='/aboutUs' sx={{fontSize:'15px'}}>About us </Button>
-          <Button href='/work' sx={{fontSize:'15px'}}>work</Button>
-          <Button href='/contact' sx={{fontSize:'15px'}}>Contacts</Button>
-        </Stack>
-      </Toolbar>
-      </Container>
-      
-    </AppBar>
-    
-  
-      
+      <AppBar position="fixed" color="grey">
+        <Container maxWidth="xl">
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <img src={Logo} alt="Logo" style={{ height: '50px', marginRight: '10px' }} />
+            <Stack direction={'row'} spacing={2} >
+              <Button
+                href='/'
+                sx={{ fontSize: '15px', fontWeight: 'none' }}
+              >home</Button>
+              <Button href='/aboutUs' sx={{ fontSize: '15px' }}>About us </Button>
+              <Button href='/work' sx={{ fontSize: '15px' }}>work</Button>
+              <Button href='/contact' sx={{ fontSize: '15px' }}>Contacts</Button>
+            </Stack>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
   )
 }
